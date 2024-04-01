7df0b94a702e71ed951f86a0141d20c0289e5518
kaphelps33 <alexphelps2001@gmail.com>
2024-04-01 09:22:15 -0400

    initial commit of swift source files

---
+++ ITCS-4102-Final/ITCS-4102-Final/ITCS_4102_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITCS_4102_FinalApp.swift
+//  ITCS-4102-Final
+//
+//  Created by Kelsey Phelps on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ITCS_4102_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
